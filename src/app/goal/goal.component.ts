3c27264aa418cbcee8b7952e696182eee8525796
Dennis-Kiambi <denniskiriinya@gmail.com>
2020-09-30 15:37:41 +0300

    add promises

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -4,6 +4,7 @@ import { GoalService } from '../goal-service/goal.service';
 import { AlertService } from '../alert-service/alert.service';
 import { HttpClient } from '@angular/common/http';
 import { Quote } from '../quote-class/quote';
+import { QuoteRequestService } from '../quote-http/quote-request.service';
 
 @Component({
   selector: 'app-goal',
@@ -45,7 +46,7 @@ export class GoalComponent implements OnInit {
     this.goals.push(goal)
   }
 
-  constructor(goalService: GoalService, alertService: AlertService, private http: HttpClient) {
+  constructor(goalService: GoalService, alertService: AlertService, private quoteService: QuoteRequestService) {
     this.goals = goalService.getGoals()
     this.alertService = alertService;
   }
@@ -57,13 +58,8 @@ export class GoalComponent implements OnInit {
       quote: string;
     }
 
-    this.http.get<ApiResponse>("http://quotes.stormconsultancy.co.uk/random.json").subscribe(data => {
-      // Succesful API request
-      this.quote = new Quote(data.author, data.quote)
-    }, err => {
-      this.quote = new Quote("Winston Churchill", "Never never give up!")
-      console.log("An error occurred")
-    })
+    this.quoteService.quoteRequest()
+    this.quote = this.quoteService.quote
   }
 
 }
